a0d29934167968bdeff8e0efc93dc280a9f2708f
Keefe <keefe.wu@outlook.com>
2019-02-19 12:54:42 +0800

    rename method

--- src/Rows.php
+++ src/Rows.php
@@ -201,7 +201,7 @@ class Rows implements \Iterator, \ArrayAccess, \Countable, \JsonSerializable
         return $this->relations[$storeKey];
     }
 
-    public function setRelations($storeKey, Rows $rows)
+    public function setRelationsRows($storeKey, Rows $rows)
     {
         $this->relations[$storeKey] = $rows;
     }
